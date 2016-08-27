03786fab66fe76b85793c6e2540eac1418f7f30f
Paulo McNally <paulomcnally@gmail.com>
2016-08-27 00:48:40 -0600

    enable makara

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 const adaro = require('adaro');
+const makara = require('makara');
+const path = require('path');
 const port = process.env.PORT || 5000;
 
 // view
-app.engine('dust', adaro.dust({}));
+let helpers = [ 'dust-makara-helpers' ];
+app.engine('dust', makara.dust({
+  cache: false,
+  helpers: helpers
+}));
 app.set('view engine', 'dust');
 app.set('views','app/views');
+
+// i18n
+app.use(makara({
+  i18n: {
+    contentPath: path.resolve(__dirname, 'locales'),
+    fallback: 'es-NI'
+  }
+}));
 
 // public
 app.use(express.static('app/assets'));
