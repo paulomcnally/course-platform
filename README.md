31931a7ca22beb5e18c951241324b143b93b5efb
Paulo McNally <paulomcnally@gmail.com>
2016-08-29 00:11:09 -0600

    multiples changes

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 
 * Redis
 * PostgreSQL
+
+## Seeds
+
+Require https://github.com/sequelize/cli
+
+    $ sequelize db:seed --seed 20160827191911-courses.js
--- seeders/20160827191911-courses.js
+++ seeders/20160827191911-courses.js
@@ -12,6 +12,146 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
         parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Negocios',
+        slug: 'business',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Informática y software',
+        slug: 'it-and-software',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Productividad en la oficina',
+        slug: 'office-productivity',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Desarrollo personal',
+        slug: 'personal-development',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Diseño',
+        slug: 'design',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Marketing',
+        slug: 'marketing',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Estilo de vida',
+        slug: 'lifestyle',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Fotografía',
+        slug: 'photography',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Salud y fitness',
+        slug: 'health-and-fitness',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Formación de profesorado',
+        slug: 'teacher-training',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Música',
+        slug: 'music',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Estudios académicos',
+        slug: 'academics',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Idioma',
+        slug: 'language',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
+      },
+      {
+        icon: 'fa-code',
+        icon_type: 'fontawesome',
+        name: 'Preparación para exámenes oficiales',
+        slug: 'test-prep',
+        active: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        parent_id: 0
       }
     ], {});
   },
